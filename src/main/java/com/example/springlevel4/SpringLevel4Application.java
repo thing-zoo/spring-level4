6782ee4beb3d5b960a1c72b8e4d67c6023d98e16
thing-zoo <cathy4025@gmail.com>
2023-07-05 11:04:00 +0900

    init commit

---
+++ src/main/java/com/example/springlevel4/SpringLevel4Application.java
@@ -0,0 +1,13 @@
+package com.example.springlevel4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLevel4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLevel4Application.class, args);
+    }
+
+}
